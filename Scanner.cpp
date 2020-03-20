2de3156a8c044ccf7b1944c0c30cd28de9052927
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2020-03-20 14:27:59 -0500

    Added the data structure of node to make a tree

--- Scanner.cpp
+++ Scanner.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+struct Node{
+	char character;     //current state character ie. state 12's character is '='
+	int terminator;     //a int/boolean (0 or 1) to determine if it is a terminator character to know if it can allow a space/tab/newline as the next character
+	Node *child;        //a single child for Node x
+	Node *sibling;      //another child for Node x
+}
+
+struct Node *start, *current;
+
 int main(int argc, char *argv[]){
 	ofstream pfile("output.txt");
 	
