5b10fa91da3b0f86a02165fba9081d3c0aa816e4
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2020-03-20 16:41:23 -0500

    Updated report and scanner

--- Scanner.cpp
+++ Scanner.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <cstring>
 
 using namespace std;
 
@@ -8,16 +10,29 @@ struct Node{
 	int terminator;     //a int/boolean (0 or 1) to determine if it is a terminator character to know if it can allow a space/tab/newline as the next character
 	Node *child;        //a single child for Node x
 	Node *sibling;      //another child for Node x
-}
+};
 
 struct Node *start, *current;
 
 int main(int argc, char *argv[]){
-	ofstream pfile("output.txt");
+	int counter = 0;
+	int chars = 100;
+	char str[chars];
+	FILE *fp;
+	fp = fopen(argv[argc - 1], "r");
 	
-	
-	pfile << "Test";
-	pfile.close();
+
+	if(fp == NULL){
+		cout << "Unable to open the file designated: " << argv[argc-1] << endl;
+		cout << "Ending Program" << endl;
+		return 1;
+	}
+	while(fgets(str, chars, fp) != NULL){
+		for(int i = 0; i < strlen(str); i++){
+			cout << i << ") " << str[i] << endl;
+		}
+	}
+	fclose(fp);
 	
 	return 0;
 }
