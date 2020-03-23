9748a6b307ad76492d31e5d72fa7d61b754a8cdd
SGO-Nova <general.n0va99@gmail.com>
2020-03-23 04:46:36 -0500

    Added tree with correct pointers

--- Scanner.cpp
+++ Scanner.cpp
@@ -2,37 +2,80 @@
 #include <fstream>
 #include <stdio.h>
 #include <cstring>
+//#include "Token_Tree.h"
 
 #define MAX_CHAR 1000
 
 using namespace std;
 
 struct tree_Node{
-	char character;       //current state character ie. state 12's character is '='
-	char state;           //current state's name
-	int terminator;       //a int/boolean (0 or 1) to determine if it is a terminator character to know if it can allow a space/tab/newline as the next character
-	tree_Node *child;     //a single child for Node x
-	tree_Node *sibling;   //another child for Node x
+	char name[10];            //current state's name
+	int state;                //current state's number
+	int terminator;           //a int/boolean (0 or 1) to determine if it is a terminator character to know if it can allow a space/tab/newline as the next character
+	tree_Node *child;         //a single child for Node x
+	tree_Node *sibling;       //another child for Node x
 };
 
 struct saved_token{
-	char token;           //name of the token
-	saved_token *next;    //next token for the file
+	char token;               //name of the token
+	saved_token *next;        //next token for the file
 };
 
 struct current_token{
-	int token;           //the character for the token(ASCII value)
-	current_token *next;  //the next character for the token.
+	int token;                //the character for the token(ASCII value)
+	current_token *next;      //the next character for the token.
 };
 
-struct tree_Node *start_tree, *current_tree;
+struct tree_Node *start_tree, *current_tree, *tmp;
 struct saved_token *start_saved, *current_saved;
 struct current_token *start_current, *current_current;
 
 void Reading_file(char *file);
 void Interpreter(char character);
+void Tree_Create(int state, char character[10], int terminator);
 
 int main(int argc, char *argv[]){
+	char arr_char[10][10] = {" ", "div", "lparen", "rparen", "plus", "minus", "times", "assign", "number", "id"};
+	
+	Tree_Create(1, arr_char[0], 0);
+	tmp = current_tree;
+	Tree_Create(2, arr_char[1], 1);
+	tmp -> child = current_tree;
+	tmp = current_tree;
+	Tree_Create(3, arr_char[0], 0);
+	tmp -> child = current_tree;
+	Tree_Create(4, arr_char[0], 0);
+	tmp -> child -> sibling = current_tree;
+	tmp = current_tree;
+	Tree_Create(5, arr_char[0], 0);
+	tmp -> child = current_tree;
+	tmp = current_tree;
+	Tree_Create(6, arr_char[2], 1);
+	start_tree -> child -> sibling = current_tree;
+	Tree_Create(7, arr_char[3], 1);
+	start_tree -> child -> sibling -> sibling = current_tree;
+	Tree_Create(8, arr_char[4], 1);
+	start_tree -> child -> sibling -> sibling -> sibling = current_tree;
+	Tree_Create(9, arr_char[5], 1);
+	start_tree -> child -> sibling -> sibling -> sibling -> sibling = current_tree;
+	Tree_Create(10, arr_char[6], 1);
+	start_tree -> child -> sibling -> sibling -> sibling -> sibling -> sibling = current_tree;
+	Tree_Create(11, arr_char[0], 0);
+	start_tree -> child -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling = current_tree;
+	tmp = current_tree;
+	Tree_Create(12, arr_char[7], 1);
+	tmp -> child = current_tree;
+	Tree_Create(13, arr_char[0], 0);
+	start_tree -> child -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling = current_tree;
+	tmp = current_tree;
+	Tree_Create(14, arr_char[8], 1);
+	start_tree -> child -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling = current_tree;
+	Tree_Create(15, arr_char[8], 1);
+	tmp -> child = current_tree;
+	start_tree -> child -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> child = current_tree;
+	Tree_Create(16, arr_char[9], 1);
+	start_tree -> child -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling -> sibling = current_tree;
+
 	FILE *fp;
 	fp = fopen(argv[argc - 1], "r");
 	char filename[MAX_CHAR];
@@ -58,6 +101,8 @@ int main(int argc, char *argv[]){
 		}
 	}
 	*/
+	cout << endl;
+	
 	return 0;
 }
 
@@ -86,4 +131,18 @@ void Interpreter(char character){
 		cout << "letter" << endl;
 	else
 		cout << "some other symbol" << endl;
+}
+
+void Tree_Create(int state, char character[10], int terminator){
+	tree_Node *temp1 = (tree_Node*)malloc(sizeof(tree_Node));	
+	for(int i = 0; i < 10; i++)
+		temp1 -> name[i] = character[i];
+	temp1 -> state = state;
+	temp1 -> terminator = terminator;
+	temp1 -> child = NULL;
+	temp1 -> sibling = NULL;
+	if(start_tree == NULL){
+		start_tree = temp1;
+	}
+	current_tree = temp1;
 }
